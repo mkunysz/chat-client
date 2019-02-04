1a202b75424196007b784fd842e7edc6c4770579
Marcin Kunysz <marcink@dna-worldwide.co.uk>
2019-02-04 14:36:06 +0100

    refactor

--- src/components/ChatConversation.vue
+++ src/components/ChatConversation.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="chat-conversation">
     <p class="chat-conversation__header">conversation</p>
-    <chat-message
+    <div
+      class="chat-conversation__message"
       v-for="message in messages"
       :message="message"
-      v-bind:key="message.id"
-    ></chat-message>
+      v-bind:key="message.id">
+      <strong class="chat-message__user">{{ message.user.name }}: </strong>
+      <span class="chat-message__content">{{ message.message }}</span>
+    </div>
   </div>
 </template>
 
 <script>
-import ChatMessage from './ChatMessage.vue';
-
 export default {
   name: 'ChatConversation',
-  components: {
-    ChatMessage
-  },
   data() {
     return {
       messages: []
@@ -38,6 +36,10 @@ export default {
 
 <style lang="scss">
 .chat-conversation {
+  &__message {
+    padding: 0 10px;
+    text-align: left;
+  }
   &__header {
     border-bottom: 1px solid #ccc;
     margin-bottom: 10px;
